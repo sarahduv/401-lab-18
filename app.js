e84a2c5d5fffc6948266ab422ee5082bbe2af5c9
sarahduv <sarah.duvdevani@gmail.com>
2019-11-23 19:09:56 -0800

    adding small change

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 'use strict';
 
-
 const fs = require('fs');
 require('./modules/read-file.js');
 require('./modules/write-file.js');
@@ -13,17 +12,16 @@ const client = new net.Socket();
 
 let file = process.argv.slice(2).shift();
 
-
-// Lifecycle
+// Lifecycle below:
 // ... we start the app here
 // read ==> (read_done / read_error)
 // on read_done ==> uppercase ==> uppercase_done
 // on uppercase_done ==> write ==> (write_done / write_error)
 
-events.on('read_done', (data) => {
+events.on('read_done', data => {
   events.emit('uppercase', data);
 });
-events.on('uppercase_done', (data) => {
+events.on('uppercase_done', data => {
   events.emit('write', data);
 });
 
